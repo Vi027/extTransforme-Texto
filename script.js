98bb43751fcf631ff4a24230fd07e371227d6f89
Vi027 <leitevitoria027@gmail.com>
2025-10-20 09:16:48 -0300

    UPDATE: json e Formato dos icons

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-/* Programa Modelo - 1 */
-window.document.addEventListener("DOMContentLoaded", function(){
-    /* Botão Letra Maiúscula */
-    window.document.querySelector("#btnMaiuscula").addEventListener("click", function(){
-        let inputText = document.querySelector("#input-text").value 
-        document.querySelector("#result").innerHTML = inputText.toUpperCase()
-    })
+window.document.addEventListener("DOMContentLoaded", function () {
+  window.document
+    .querySelector("#btnMaiuscula")
+    .addEventListener("click", function () {
+      let inputText = document.querySelector("#input-text").value;
+      document.querySelector("#result").value = inputText.toUpperCase();
+    });
 
-    /* Botão Letra Maiúscula */
-    window.document.querySelector("#btnMinuscula").addEventListener("click", function(){
-        let inputText = document.querySelector("#input-text").value 
-        document.querySelector("#result").innerHTML = inputText.toLowerCase()
-    })
-})
+  window.document
+    .querySelector("#btnMinuscula")
+    .addEventListener("click", function () {
+      let inputText = document.querySelector("#input-text").value;
+      document.querySelector("#result").value = inputText.toLowerCase();
+    });
+});
